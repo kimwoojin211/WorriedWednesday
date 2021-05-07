69e4b826a0c15e4ead6ff8dab13e69174146ec8d
Woo Jin Kim <300hHZ@gmail.com>
2021-05-07 12:40:35 -0700

    add LoginViewModel base logic and LoginPage bindings

--- WorriedWednesday/WorriedWednesday/ViewModel/LoginViewModel.cs
+++ WorriedWednesday/WorriedWednesday/ViewModel/LoginViewModel.cs
@@ -1,10 +1,91 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.ComponentModel;
+using Xamarin.Forms;
+using System.Windows.Input;
+using WorriedWednesday.Views;
+
 
 namespace WorriedWednesday.ViewModel
 {
-    class LoginViewModel
+  public class LoginViewModel : INotifyPropertyChanged
+  {
+    string name, email, password;
+
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    public LoginViewModel()
+    {
+      LoginCommand = new Command(Login);
+      RegisterCommand = new Command(Register);
+    }
+    public ICommand LoginCommand { get; set; }
+    public ICommand RegisterCommand { get; set; }
+
+    void Register(object parameter)
+    {
+
+    }
+
+    void Login(object parameter)
     {
     }
+    void OnPropertyChanged(string propertyName)
+    {
+      PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+    public string Name
+    {
+      get
+      {
+        return name;
+      }
+      set
+      {
+        name = value;
+        OnPropertyChanged("Name");
+      }
+    }
+
+    public string Email
+    {
+      get
+      {
+        return email;
+      }
+      set
+      {
+        email = value;
+        OnPropertyChanged("Email");
+      }
+    }
+
+    public string Password
+    {
+      get
+      {
+        return password;
+      }
+      set
+      {
+        password = value;
+        OnPropertyChanged("Password");
+
+      }
+    }
+
+    string confirmPassword;
+    public string ConfirmPassword
+    {
+      get
+      {
+        return confirmPassword;
+      }
+      set
+      {
+        confirmPassword = value;
+      }
+    }
+  }
 }
